c483e7c8a03e17cddd9c9fac755a07f8ef4134fb
Deepak Shah <shah2@stolaf.edu>
2015-09-17 20:48:22 -0500

    HW2 Complete

---
+++ hw2/sqrts.C
@@ -0,0 +1,17 @@
+/* print square roots in C++ language.  R. Brown, 9/2010 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+main()
+{
+  cout << "n\tsqrt(n)" << endl << "----------------" << endl;
+  for (int n=0;  n<=5;  n++){
+  	cout<<n;
+  	cout << "\t"<<sqrt(n) << endl;
+}
+  return 0;
+}
+
