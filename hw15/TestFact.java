20762b4bd40d6e70f8c2aa6f43e927661f9bd24a
Deepak Shah <shah2@stolaf.edu>
2015-10-18 00:44:59 -0500

    HW 15 Complete

---
+++ hw15/TestFact.java
@@ -0,0 +1,15 @@
+
+
+class TestFact{
+	public static void main(String[] args){
+		System.out.println(factorial(10));
+	}
+
+public static int factorial(int n){
+		if(n==0)
+			return 1;
+			else
+				return (n*factorial(n-1));
+	
+	}
+}
